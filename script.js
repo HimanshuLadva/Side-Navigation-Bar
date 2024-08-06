f7a72ad1f549bb96e1e23667948c2bb4fadfcb5d
HimanshuLadva <himanshuladva2241@gmail.com>
2024-08-06 21:37:01 +0530

    now matched with theme

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 const body = document.querySelector("body");
-// const darkLight = document.querySelector("#darkLight");
 const sidebar = document.querySelector(".sidebar");
 const submenuItems = document.querySelectorAll(".submenu_item");
-// const sidebarOpen = document.querySelector("#sidebarOpen");
 const sidebarClose = document.querySelector(".collapse_sidebar");
 const sidebarExpand = document.querySelector(".expand_sidebar");
-// sidebarOpen.addEventListener("click", () => sidebar.classList.toggle("close"));
 
 sidebarClose.addEventListener("click", () => {
   sidebar.classList.add("close", "hoverable");
@@ -24,16 +21,6 @@ sidebar.addEventListener("mouseleave", () => {
     sidebar.classList.add("close");
   }
 });
-
-// darkLight.addEventListener("click", () => {
-//   body.classList.toggle("dark");
-//   if (body.classList.contains("dark")) {
-//     document.setI;
-//     darkLight.classList.replace("bx-sun", "bx-moon");
-//   } else {
-//     darkLight.classList.replace("bx-moon", "bx-sun");
-//   }
-// });
 
 submenuItems.forEach((item, index) => {
   item.addEventListener("click", () => {
